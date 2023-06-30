ce21a408556f5d0e2d0b3c17f4c229f1995325c4
Eusebie Siebenberg <siebenee@gmail.com>
2023-06-30 10:13:35 -0700

    Add CoffeeList.js component

---
+++ src/components/CoffeeList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Coffee from "./Coffee";
+import PropTypes from "prop-types";
+
+function CoffeeList(props){
+  return (
+    <React.Fragment>
+      <hr/>
+        {props.coffeeList.map((coffee) =>
+          <Coffee 
+          whenCoffeeClicked = { props.onCoffeeSelection }
+          name={coffee.name}
+          origin={coffee.origin}
+          roast={coffee.roast}
+          price={coffee.price}
+          available={coffee.available}
+          sold={coffee.sold}
+          id={coffee.id}
+          key={coffee.id}/>
+        )}
+    </React.Fragment>
+  );
+}
+
+CoffeeList.propTypes = {
+  coffeeList: PropTypes.array,
+  onCoffeeSelection: PropTypes.func
+};
+
+export default CoffeeList;
