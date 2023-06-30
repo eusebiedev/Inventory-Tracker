1365a2e7c774d68c6a4a345785b069c03bb16778
Eusebie Siebenberg <siebenee@gmail.com>
2023-06-30 11:22:37 -0700

    Create EditCoffeeForm.js component

---
+++ src/components/EditCoffeeForm.js
@@ -0,0 +1,34 @@
+import React from "react";
+import ReusableForm from "./ReusableForm";
+import PropTypes from "prop-types";
+
+
+function EditCoffeeForm (props) {
+  const { coffee } = props;
+
+  function handleEditCoffeeFormSubmission(event) {
+    event.preventDefault();
+    props.onEditCoffee({
+      name: event.target.name.value, 
+      origin: event.target.origin.value,
+      roast: event.target.roast.value, 
+      price: parseFloat(event.target.price.value), 
+      available: parseInt(event.target.available.vaule),
+      id: coffee.id});
+  }
+  
+  return (
+    <React.Fragment>
+      <ReusableForm 
+        formSubmissionHandler={handleEditCoffeeFormSubmission}
+        buttonText="Update Coffee" />
+    </React.Fragment>
+  );
+}
+
+EditCoffeeForm.propTypes = {
+  coffee: PropTypes.object,
+  onEditCoffee: PropTypes.func
+};
+
+export default EditCoffeeForm;
