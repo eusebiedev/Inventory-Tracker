5f74ed0ec501fcaf5ee4fec09ccd86bc293a4859
Eusebie Siebenberg <siebenee@gmail.com>
2023-06-30 10:08:54 -0700

    update header with img

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import React from "react";
+import headerImage from "./../img/coffee.jpg";
 
 function Header(){
+
   return (
     <React.Fragment>
-      <h1>The Burlap Bean Tracker</h1>
-      <hr/>
+      <div className="header">
+        <h1>The Burlap Bean Tracker</h1>
+        <img src={headerImage} alt="coffee" />
+        <hr/>
+      </div>
     </React.Fragment>
   );
 }
