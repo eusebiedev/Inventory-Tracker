80ee132a0d1428b1748a2b99b77ddefa5d00a6cd
Eusebie Siebenberg <siebenee@gmail.com>
2023-06-30 11:47:47 -0700

    Create NewCoffeeForm.js component

---
+++ src/components/NewCoffeeForm.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import { v4 } from 'uuid';
+import ReusableForm from "./ReusableForm";
+
+function NewCoffeeForm(props) {
+
+  function handleNewCoffeeFormSubmission(event) {
+    event.preventDefault();
+    props.onNewCoffeeCreation({
+      name: event.target.name.value, 
+      origin: event.target.origin.value,
+      roast: event.target.roast.value, 
+      price: parseInt(event.target.price.value), 
+      available: parseInt(event.target.available.vaule),
+      id: v4()
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm 
+        formSubmissionHandler={handleNewCoffeeFormSubmission}
+        buttonText="Add New Coffee!" />
+    </React.Fragment>
+  );
+}
+
+NewCoffeeForm.propTypes = {
+  onNewCoffeeCreation: PropTypes.func
+};
+
+export default NewCoffeeForm;
