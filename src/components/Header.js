27831c2208fdfcbe1a2fad960b02c30e256fda15
Eusebie Siebenberg <siebenee@gmail.com>
2023-06-30 08:59:58 -0700

    Add Header.js component with title

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+  return (
+    <React.Fragment>
+      <h1>The Burlap Bean Tracker</h1>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+export default Header; 
